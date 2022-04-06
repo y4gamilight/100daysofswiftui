6a55435d7ab49df396f187cd6ff997fbd6db87a3
C02GM15YQ05R <C02GM15YQ05R@ThomasLes-MacBook-Pro.local>
2022-04-06 17:56:35 +0700

    -Import source code

---
+++ SwiftUI100days/Shared/SwiftUI100daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100daysApp.swift
+//  Shared
+//
+//  Created by C02GM15YQ05R on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
