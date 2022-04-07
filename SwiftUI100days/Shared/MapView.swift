3663a14e9a6ca62564741aeae8a46828ae732f05
C02GM15YQ05R <C02GM15YQ05R@ThomasLes-MacBook-Pro.local>
2022-04-07 18:06:26 +0700

    Add mapview and small example

---
+++ SwiftUI100days/Shared/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  SwiftUI100days
+//
+//  Created by C02GM15YQ05R on 07/04/2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_866),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
